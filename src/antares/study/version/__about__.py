35f6ae1c880050fa6b8d03266050783b1b78ef89
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-04-10 08:21:27 +0200

    build: prepare version v0.1.1 (2024-04-10)

--- src/antares/study/version/__about__.py
+++ src/antares/study/version/__about__.py
@@ -4,7 +4,7 @@ Antares Study (and Solver) version models.
 
 # Standard project metadata
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2024-03-20"
+__date__ = "2024-04-10"
 __credits__ = "© Réseau de Transport de l’Électricité (RTE)"
