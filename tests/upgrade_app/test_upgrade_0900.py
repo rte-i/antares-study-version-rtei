9781cd7730c3bcfa0aa8d3218d38ceb681cf35d8
Florian OMNES <26088210+flomnes@users.noreply.github.com>
2024-08-08 13:48:52 +0200

    Format

--- tests/upgrade_app/test_upgrade_0900.py
+++ tests/upgrade_app/test_upgrade_0900.py
@@ -1,15 +1,16 @@
-#from antares.study.version.upgrade_app.upgrader_0900 import UpgradeTo0900
+# from antares.study.version.upgrade_app.upgrader_0900 import UpgradeTo0900
 from antares.study.version.upgrade_app import UpgradeApp
 from antares.study.version import StudyVersion
 from antares.study.version.ini_reader import IniReader
 from tests.conftest import StudyAssets
 
 
-def get_version(d): # type: ignore
+def get_version(d):  # type: ignore
     """
     Extract version attribute from a nested dict structure
     """
     return d["antares"]["version"]
+
 
 def test_nominal_case(study_assets: StudyAssets):
     """
@@ -17,9 +18,9 @@ def test_nominal_case(study_assets: StudyAssets):
     """
 
     # upgrade the study
-    app = UpgradeApp(study_assets.study_dir, version=StudyVersion(9,0))  # type: ignore
+    app = UpgradeApp(study_assets.study_dir, version=StudyVersion(9, 0))  # type: ignore
     app()
-    
+
     actual_path = study_assets.study_dir.joinpath("study.antares")
     actual = IniReader().read(actual_path)
     expected_path = study_assets.expected_dir.joinpath("study.antares")
