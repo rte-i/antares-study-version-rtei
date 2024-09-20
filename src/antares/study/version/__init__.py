acb56e83f5ffbec7bc536ad1edce4f4952b7dc60
MartinBelthle <martin.belthle@rte-france.com>
2024-09-20 14:09:32 +0200

    avoid mypy warning when importing classes (#14)

--- src/antares/study/version/__init__.py
+++ src/antares/study/version/__init__.py
@@ -4,3 +4,5 @@ Antares Study (and Solver) version models.
 
 from .converters import version_to_triplet  # noqa: F401
 from .model import SolverVersion, StudyVersion  # noqa: F401
+
+__all__ = ("SolverVersion", "StudyVersion")
