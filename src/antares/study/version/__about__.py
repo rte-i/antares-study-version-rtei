9d8371a01686610429d302f384107bc88c1c7c25
MartinBelthle <martin.belthle@rte-france.com>
2024-10-15 14:45:55 +0200

    v1.0.8 (#21)

--- src/antares/study/version/__about__.py
+++ src/antares/study/version/__about__.py
@@ -4,7 +4,7 @@ Antares Study (and Solver) version models.
 
 # Standard project metadata
 
-__version__ = "1.0.7"
+__version__ = "1.0.8"
 __author__ = "RTE, Antares Web Team"
-__date__ = "2024-07-06"
+__date__ = "2024-10-15"
 __credits__ = "© Réseau de Transport de l’Électricité (RTE)"
