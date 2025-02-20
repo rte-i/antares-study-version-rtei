ff3b15269b0d9355cf2664562fd2d1c761138ad1
MartinBelthle <martin.belthle@rte-france.com>
2025-02-20 10:01:55 +0100

    fix(resources): remove usage of importlib.resources (#27)

--- src/antares/study/version/create_app/__init__.py
+++ src/antares/study/version/create_app/__init__.py
@@ -2,14 +2,14 @@ import dataclasses
 import datetime
 import typing as t
 import zipfile
-from importlib.resources import contents, open_binary
 from pathlib import Path
 
 from antares.study.version.exceptions import ApplicationError
 from antares.study.version.model.study_antares import StudyAntares
 from antares.study.version.model.study_version import StudyVersion
 
-_RESOURCES_PACKAGE = "antares.study.version.create_app.resources"
+HERE = Path(__file__).resolve()
+_RESOURCES_PATH = HERE.parent / "resources"
 
 
 def get_template_version(template_name: str) -> StudyVersion:
@@ -18,9 +18,10 @@ def get_template_version(template_name: str) -> StudyVersion:
     return StudyVersion.parse(version_str)
 
 
-TEMPLATES_BY_VERSIONS: t.Dict[StudyVersion, str] = {
-    get_template_version(name): name for name in contents(_RESOURCES_PACKAGE) if name.endswith(".zip")
-}
+TEMPLATES_BY_VERSIONS: t.Dict[StudyVersion, str] = {}
+for resource in _RESOURCES_PATH.iterdir():
+    if resource.name.endswith(".zip"):
+        TEMPLATES_BY_VERSIONS[get_template_version(resource.name)] = resource.name
 
 
 def available_versions() -> t.List[str]:
@@ -61,10 +62,9 @@ class CreateApp:
             msg = f"No available template for version {self.version}: available templates are {available_versions()}"
             raise ApplicationError(msg)
         print(f"Extracting template {template_name} to '{self.study_dir}'...")
-        with open_binary(_RESOURCES_PACKAGE, template_name) as zip_file:
-            with zipfile.ZipFile(zip_file, mode="r") as archive:
-                archive.extractall(self.study_dir)
-
+        resource_path = _RESOURCES_PATH / template_name
+        with zipfile.ZipFile(resource_path, mode="r") as archive:
+            archive.extractall(self.study_dir)
         creation_date = datetime.datetime.now()
         study_antares = StudyAntares(
             version=self.version,
