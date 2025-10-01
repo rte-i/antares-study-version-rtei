a8b6fc266f1c41e75bfc1e76d401aeb0528923a9
dusanparipovic <dusan.paripovic@rte-international.com>
2025-10-01 16:03:48 +0200

    refactor(upgrade 9.2): remove unnecessary lines of code

--- src/antares/study/version/upgrade_app/upgrader_0902.py
+++ src/antares/study/version/upgrade_app/upgrader_0902.py
@@ -114,8 +114,6 @@ class UpgradeTo0902(UpgradeMethod):
 
         common_capacity_path = hydro_dir / "common" / "capacity"
 
-        if not Path(common_capacity_path).is_dir():
-            return
 
         for area_id in all_areas_ids:
             gen_file = common_capacity_path / f"maxDailyGenEnergy_{area_id}.txt"
@@ -131,8 +129,6 @@ class UpgradeTo0902(UpgradeMethod):
 
         series_path = hydro_dir / "series"
 
-        if not Path(series_path).is_dir():
-            return
         for area in series_path.iterdir():
             area_dir = hydro_dir / area
             for matrix in matrices_to_create:
